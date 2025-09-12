56601e262e9cddbcacf5ea73729d942398aa650d
sternbergh <sternbergharrison04@gmail.com>
2025-09-12 18:15:36 +0000

    upa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-a
+document.querySelectorAll('.tab-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const tabId = button.getAttribute('data-tab');
+
+    // Hide all tabs
+    document.querySelectorAll('.tab-content').forEach(tab => {
+      tab.style.display = 'none';
+    });
+
+    // Show selected tab
+    document.getElementById(tabId).style.display = 'block';
+  });
+});
